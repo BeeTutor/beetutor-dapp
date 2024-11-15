a4d70d6b8dedacdf1575432239b10b6d407c8c34
White <t100210022002@gmail.com>
2024-11-16 05:53:44 +0700

    feat: add heney token

--- server/config.js
+++ server/config.js
@@ -15,4 +15,14 @@ export const configMap = {
       airDrop: process.env.ACTION_ID,
     },
   },
+  contract: {
+    providerUrl:
+      process.env.PROVIDER_URL ||
+      "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+    honeyTokenContract: process.env.HONEY_TOKEN_CONTRACT_ADDRESS,
+    honeyTokenAirDropAmount: process.env.HONEY_TOKEN_AIR_DROP_AMOUNT || 999,
+  },
+  admin: {
+    privateKey: process.env.PRIVATE_KEY,
+  },
 };
--- server/controller/token.controller.js
+++ server/controller/token.controller.js
@@ -1,15 +1,18 @@
 import { verifyCloudProof } from "@worldcoin/idkit";
 import { configMap } from "../config.js";
+import { contractService } from "../service/contract.service.js";
 
 class TokenController {
   claimAirDropToken = async (req, res) => {
-    const proof = req.body;
+    const { proof, to } = req.body;
+
     const app_id = configMap.wordCoin.appId;
     const action = configMap.wordCoin.actionId.airDrop;
     const verifyRes = await verifyCloudProof(proof, app_id, action);
     if (verifyRes.success) {
       // This is where you should perform backend actions if the verification succeeds
       // Such as, setting a user as "verified" in a database
+      await contractService.claimAirDrop(to);
       res.status(200).send(verifyRes);
     } else if (verifyRes.code === "max_verifications_reached") {
       res.status(400).json({
