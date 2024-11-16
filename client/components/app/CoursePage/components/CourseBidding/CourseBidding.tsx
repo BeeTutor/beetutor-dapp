01a653e765d139d07462fad33fdc7b8ac9cbc0bb
White <t100210022002@gmail.com>
2024-11-17 05:35:10 +0700

    feat: fix build

--- client/components/app/CoursePage/components/CourseBidding/CourseBidding.tsx
+++ client/components/app/CoursePage/components/CourseBidding/CourseBidding.tsx
@@ -112,14 +112,14 @@ export const CourseBidding: React.FC<Props> = ({ ...gridProps }) => {
           Place your bid.
           <InputGroup startElement="$">
             <Input
-              // disabled={nowSessionStatus !== "open"}
+              disabled={!nowSessionStatus}
               type="number"
               value={bidValue}
               onChange={(e) => setBidValue(e.target.value)}
             />
           </InputGroup>
           <Button
-            // disabled={nowSessionStatus !== "open"}
+            disabled={!nowSessionStatus}
             onClick={async () => {
               const amountInWei = ethers.parseUnits(bidValue, "ether");
               console.log("Place Bid:", amountInWei);
